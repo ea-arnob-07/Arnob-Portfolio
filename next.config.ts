224654f8b1ffd033708c391cde79a5ae2a181ccd
ea-arnob-07 <eaarnob178@gmail.com>
2026-07-26 17:07:08 +0600

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Arnob-Portfolio",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
